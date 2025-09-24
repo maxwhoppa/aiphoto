5198c7bd51c93aa180edfa8584ac729a5ac5e403
maxwhoppa <maxwellsawesome@gmail.com>
2025-09-24 22:39:41 +0200

    Fix ESLint config to use basic rules only
    Removed TypeScript-specific extends that require missing packages.

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -3,23 +3,19 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2022,
     sourceType: 'module',
-    project: './tsconfig.json',
   },
   plugins: ['@typescript-eslint'],
   extends: [
     'eslint:recommended',
-    '@typescript-eslint/recommended',
   ],
   env: {
     node: true,
     es2022: true,
   },
   rules: {
-    // Disable some strict rules for now
-    '@typescript-eslint/no-explicit-any': 'warn',
-    '@typescript-eslint/no-unused-vars': 'warn',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    // Basic rules only
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
   },
   ignorePatterns: [
     'dist/',
