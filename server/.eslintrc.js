2b9b8d9448a51f0e0d67fd852ccd71e61fb0d275
maxwhoppa <maxwellsawesome@gmail.com>
2025-09-24 22:35:43 +0200

    drizzle

---
+++ server/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    '@typescript-eslint/recommended',
+  ],
+  env: {
+    node: true,
+    es2022: true,
+  },
+  rules: {
+    // Disable some strict rules for now
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  },
+  ignorePatterns: [
+    'dist/',
+    'node_modules/',
+    'drizzle/',
+    '*.js',
+    '*.d.ts',
+  ],
+};
