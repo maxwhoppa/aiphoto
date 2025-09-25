6a32567c77ee8362bd946279b4069adf4e47e05a
maxwhoppa <maxwellsawesome@gmail.com>
2025-09-25 03:16:49 +0200

    rm the old server, cya nerd

--- server-old/src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { router } from '@/trpc';
-import { imagesRouter } from './images';
-
-export const appRouter = router({
-  images: imagesRouter,
-});
-
-export type AppRouter = typeof appRouter;
